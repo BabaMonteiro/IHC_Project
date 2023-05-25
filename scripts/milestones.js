634e3282550dda1907aa6e2ce6ca8924266459b4
BabaMonteiro <barbarammoreira@ua.pt>
2023-05-25 19:48:13 +0100

    +

--- scripts/milestones.js
+++ scripts/milestones.js
@@ -16,10 +16,8 @@ window.onload = () => {
 
 const trophyIcons = document.querySelectorAll('.trophy-pair i');
 
-// Add click event listener to each trophy icon
 trophyIcons.forEach(icon => {
   icon.addEventListener('click', () => {
-    // Toggle the 'clicked' class on the clicked trophy icon
     icon.classList.toggle('clicked');
   });
 });
